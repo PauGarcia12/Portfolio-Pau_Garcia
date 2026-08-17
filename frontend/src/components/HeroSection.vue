cb3994d5246fca596354a382278f7aa49ec224fe
PauGarcia77 <pgarcia15@nigul.cide.es>
2026-08-17 18:33:25 +0200

    Mejorar calidad imagen, y añadir botones de contacto

--- frontend/src/components/HeroSection.vue
+++ frontend/src/components/HeroSection.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col md:flex-row items-center gap-10">
         <div class="flex-shrink-0">
           <div class="w-28 h-28 md:w-32 md:h-32 rounded-full overflow-hidden border border-black/10 bg-neutral-100">
-            <img src="/pau.jpg" alt="Pau García" class="w-full h-full object-cover" />
+            <img src="/pau.jpg" alt="Pau García" class="w-full h-full object-cover object-top" />
           </div>
         </div>
 
